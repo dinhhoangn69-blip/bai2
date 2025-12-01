3271f11877e4d05ff98e957cdc5cd0407246db09
dinhhoangn69-blip <dinhhoangn69@gmail.com>
2025-12-01 08:17:52 +0700

    bai2_3

---
+++ bài2_3.py
@@ -0,0 +1,10 @@
+print("Sinh vien:Nguyen Dinh Hoang")
+print("Ma so SV : 245752021610043")
+print("###########################")
+#################################
+n = int(input("Enter a number ---> "))
+
+if n % 2 == 0:
+    print("EVEN Number")
+else:
+    print("ODD Number")
