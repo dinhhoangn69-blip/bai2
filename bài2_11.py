4fc0ba49d767e342de5069699c6a03013d28f210
dinhhoangn69-blip <dinhhoangn69@gmail.com>
2025-12-01 08:29:27 +0700

    bai2_11

---
+++ bài2_11.py
@@ -0,0 +1,10 @@
+print("Sinh vien: Nguyen Dinh Hoang")
+print("Ma so SV : 245752021610043")
+print("###########################")
+#################################
+l = [1, 'python', 4, 7]
+s = ['cse', 2, 'guntur', 8]
+
+m = dict(zip(l, s))
+
+print("Combined dictionary:", m)
