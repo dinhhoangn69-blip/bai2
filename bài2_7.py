2c4ccc6a5a9b429ea88fc9a866933883e351d986
dinhhoangn69-blip <dinhhoangn69@gmail.com>
2025-12-01 08:25:42 +0700

    bai2_7

---
+++ bài2_7.py
@@ -0,0 +1,9 @@
+print("Sinh vien: Nguyen Dinh Hoang")
+print("Ma so SV : 245752021610043")
+print("###########################")
+#################################
+n = int(input("Nhập vào một số: "))
+d = dict()
+
+for i in range(1, n + 1):
+    d[i] = i * i
