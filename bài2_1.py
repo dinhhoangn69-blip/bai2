f22958ecf50a4551f2573d7527b11704ad26d286
dinhhoangn69-blip <dinhhoangn69@gmail.com>
2025-12-01 08:13:04 +0700

    bai2_1

---
+++ bài2_1.py
@@ -0,0 +1,12 @@
+print("Sinh vien:Nguyen Dinh Hoang")
+print("Ma so SV : 245752021610043")
+print("###########################")
+#################################
+n1 = int(input("Enter n1 value: "))
+n2 = int(input("Enter n2 value: "))
+
+if n1 > n2:
+    print("n1 is big")
+else:
+    print("n2 is big")
+
