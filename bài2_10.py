740c9bfc6441e62b54b3575e1fe8d51ffbedb44e
dinhhoangn69-blip <dinhhoangn69@gmail.com>
2025-12-01 08:28:05 +0700

    bai2_10

---
+++ bài2_10.py
@@ -0,0 +1,11 @@
+print("Sinh vien: Nguyen Dinh Hoang")
+print("Ma so SV : 245752021610043")
+print("###########################")
+#################################
+a = "Hi I am python programmer"
+
+b = a.split()     
+print("After split:", b)
+
+c = "-".join(b)    
+print("After join:", c)
